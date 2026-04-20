5d9e32d6d2a8389e4b9bbfecb84dc7208d0536f3
TRALEDON <yuldashevasror759@gmail.com>
2026-04-21 04:10:43 +1000

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css"
 const App = () => {
   return (
     <div>
-      <nav><img src="" alt="" /> <h1 className='h1-nav'>home</h1> <h1 className='h1-nav'>about us</h1> <h1 className='h1-nav'>portfilio</h1><h1 className='h1-nav'>contact</h1></nav>
+      <nav><img src="" alt="" /> <h1 className='1-nav'>home</h1> <h1 className='1-nav'>about us</h1> <h1 className='1-nav'>portfilio</h1><h1 className='1-nav'>contact</h1></nav>
     </div>
   )
 }
