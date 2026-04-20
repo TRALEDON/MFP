0f322589f8569ce3eaa5de3d31ae94901184c1f4
TRALEDON <yuldashevasror759@gmail.com>
2026-04-21 04:42:56 +1000

    3commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import "./App.css"
+import lg from "./assets/logo.png"
+import i from "./assets/i'm.png"
 const App = () => {
   return (
     <div>
-      <nav><img src="" alt="" /> <h1 className='1-nav'>home</h1> <h1 className='1-nav'>about us</h1> <h1 className='1-nav'>portfilio</h1><h1 className='1-nav'>contact</h1></nav>
+      <nav><img src={lg} alt="" /> <h1 className='h1-nav'>home</h1> <h1 className='h1-nav'>about us</h1> <h1 className='h1-nav'>portfilio</h1><h1 className='h1-nav'>contact</h1></nav>
+      <main><h1 className='h-m'>Hello i'm gleb kostrunov</h1><p className='p-m'>I've been doing web design, front-end and back-end development for a year now. Do you need a website design, site layout, or maybe a turnkey website? Then contact me</p><button className='M-B'>contack</button></main>
     </div>
   )
 }
