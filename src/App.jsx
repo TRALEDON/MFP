9b28f8304fdf1cb4cee79352b0ce44df1226aaaa
TRALEDON <yuldashevasror759@gmail.com>
2026-04-21 04:50:15 +1000

    4 commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ const App = () => {
   return (
     <div>
       <nav><img src={lg} alt="" /> <h1 className='h1-nav'>home</h1> <h1 className='h1-nav'>about us</h1> <h1 className='h1-nav'>portfilio</h1><h1 className='h1-nav'>contact</h1></nav>
-      <main><h1 className='h-m'>Hello i'm gleb kostrunov</h1><p className='p-m'>I've been doing web design, front-end and back-end development for a year now. Do you need a website design, site layout, or maybe a turnkey website? Then contact me</p><button className='M-B'>contack</button></main>
+      <main> <div className='d-m'><h1 className='h-m'>Hello i'm gleb kostrunov</h1><p className='p-m'>I've been ding web design, front-end and back-end development for a year now. Do you need a website design, site layout, or maybe a turnkey website? Then contact me</p><button className='M-B'>contack</button></div>
+      <div className='dd-m'><img src={i} alt="" /></div></main>
     </div>
   )
 }
