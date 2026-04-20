aefc96bccaa48ee9b7328626a9405494bf09777f
TRALEDON <yuldashevasror759@gmail.com>
2026-04-21 04:04:39 +1000

    2 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import "./App.css"
 const App = () => {
   return (
     <div>
-      <nav></nav>
+      <nav><img src="" alt="" /> <h1 className='h1-nav'>home</h1> <h1 className='h1-nav'>about us</h1> <h1 className='h1-nav'>portfilio</h1><h1 className='h1-nav'>contact</h1></nav>
     </div>
   )
 }
